8b7035855d9c2b34d99064243b3591f380c3b8f5
johnmojica1892 <johnmojica1892@gmail.com>
2020-12-09 15:32:31 +0800

    lots of changes

--- app/src/main/java/com/example/classattendance/Register.java
+++ app/src/main/java/com/example/classattendance/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,16 +15,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+    public static final String TAG = "TAG";
     EditText mFullName,mEmail,mPassword,mStudentNumber;
     Button mRegisterBtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +49,9 @@ public class Register extends AppCompatActivity {
         mLoginBtn = findViewById(R.id.txtLoginHere);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressBarRgstr);
+
 
         if (fAuth.getCurrentUser() != null) {
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
@@ -48,12 +61,22 @@ public class Register extends AppCompatActivity {
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String fullName = mFullName.getText().toString();
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                String studentNumber = mStudentNumber.getText().toString();
+
+
+
+                if(TextUtils.isEmpty(fullName)) {
+                    mFullName.setError("This field is required");
+                    return;
+                }
 
                 if(TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is required");
                     return;
+
                 }
 
                 if(TextUtils.isEmpty(password)) {
@@ -66,24 +89,48 @@ public class Register extends AppCompatActivity {
                     return;
                 }
 
+                if (TextUtils.isEmpty(studentNumber)) {
+                    mStudentNumber.setError("Student Number is required");
+                    return;
+                }
+
                 progressBar.setVisibility(View.VISIBLE);
 
                 // Register the user in firebase
 
-                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                         if (task.isSuccessful()) {
                             Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("fName", fullName);
+                            user.put("email",email);
+                            user.put("idNumber", studentNumber);
+                            documentReference.set(user).addOnSuccessListener((OnSuccessListener) (aVoid) -> {
+                                    Log.d(TAG, "onSuccess: user Profile is Created for "+ userID);
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: " + e.toString());
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
 
                         }else {
                             Toast.makeText(Register.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-
-                    }
+                         }
+                            progressBar.setVisibility(View.GONE);
                 });
             }
         });
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/classattendance/TimeInandOut.java
@@ -0,0 +1,14 @@
+package com.example.classattendance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimeInandOut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_inand_out);
+    }
+}
